81738ecce4c520148346748c6b3feb140d154a95
Olemis Lang <olemis+work@gmail.com>
2018-10-29 22:07:38 -0400

    [ci] refs #1069 - Print messages for each step needed to brew install gcc6

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -8,7 +8,7 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew update
   echo 'Available versions (gcc)' && brew list --versions gcc
-  brew list gcc@6 &>/dev/null || ( cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core" && git checkout $HOMEBREW_CORE_VERSION && brew install gcc@6 )
+  brew list gcc@6 &>/dev/null || (echo "Check out version $HOMEBREW_CORE_VERSION" && cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core" && git checkout $HOMEBREW_CORE_VERSION && echo "Install gcc@6" &&  brew install gcc@6 )
 fi
 
 cd $TRAVIS_BUILD_DIR

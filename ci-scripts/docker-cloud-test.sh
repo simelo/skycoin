79fabf460f70906254a9e11a49fc24e1df0fde29
stdevAdrianPaez <a.paez@estudiantes.matcom.uh.cu>
2019-05-05 17:25:03 -0400

    [docker] refs #1433 Uploading files for autotests

---
+++ ci-scripts/docker-cloud-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export PORT=6420
+cd ./..
+DIR=$PWD
+
+
+TESTS=(
+    'integration-test-stable'
+    'integration-test-stable-disable-csrf'
+    'integration_test-stable-disable-wallet-api'
+    'integration-test-enable-seed-api'
+    'integration-test-disable-gui'
+    'integration-test-auth'
+    'integration-test-db-no-unconfirmed'
+)
+
+for TEST in ${TESTS[@]} ; do
+    echo "----- START TEST: $TEST -----"
+    export SKYCOIN_NODE="$TEST"
+    make $TEST
+    FAIL=$?
+    if [ $FAIL -ne 0 ]; then
+        echo "----- FAIL TEST: $TEST -----"
+        # exit 1
+    fi
+    echo "----- PASS TEST: $TEST -----"
+
+    cd /wallet
+    rm -r -f `ls`
+    cd /data/.skycoin
+    rm -r -f `ls`
+    cd $DIR
+done

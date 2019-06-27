b59a04f5bca656098ec0dcad33afce6d03f43c73
Mauricio Perdomo <ml.perdomo97@gmail.com>
2019-06-27 15:30:45 -0400

    [docker] fix test in docker cloud

--- ci-scripts/docker-cloud-test.sh
+++ ci-scripts/docker-cloud-test.sh
@@ -10,10 +10,10 @@ curl http://integration-test-stable:6420/api/v1/version
 
 curl http://integration-test-disable-wallet-api:6420/api/v1/version
  
-
+#'integration-test-stable'
+#'integration-test-stable-disable-csrf'
 TESTS=(
-    'integration-test-stable'
-    'integration-test-stable-disable-csrf'
+    
     'integration-test-disable-wallet-api'
     'integration-test-enable-seed-api'
     'integration-test-disable-gui'
@@ -32,7 +32,7 @@ for TEST in ${TESTS[@]} ; do
     fi
     ln -s /data/.skycoin-$TEST /data/.skycoin
     
-    if [ $TEST -ne 'integration-test-disable-wallet-api' ]; then
+    if [ $TEST != 'integration-test-disable-wallet-api' ]; then
         ln -s /wallet-$TEST /wallet
     fi
 
